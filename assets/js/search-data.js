8c342563b75f6b34cf38ebcf438a9683642ec001
andizimmerer <andreas-zimmerer@outlook.de>
2026-08-25 13:46:45 +0000

    Deploying to gh-pages from @ utndatasystems/bdbd-2025-website@acc3795bd3c498efc5df2cf9abed91006aa597ee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "About",
+    id: "nav-",
+    title: "",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -14,21 +14,42 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/call-for-abstracts/";
+            window.location.href = "/2026/call-for-abstracts/";
+          },
+        },{id: "nav-call-for-abstracts",
+          title: "Call for Abstracts",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/2025/call-for-abstracts/";
           },
         },{id: "nav-program",
           title: "Program",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/program/";
+            window.location.href = "/2026/program/";
+          },
+        },{id: "nav-program",
+          title: "Program",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/2025/program/";
           },
         },{id: "nav-participation",
           title: "Participation",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/participation/";
+            window.location.href = "/2026/participation/";
+          },
+        },{id: "nav-participation",
+          title: "Participation",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/2025/participation/";
           },
         },{id: "news-our-website-is-now-live",
           title: 'Our website is now live.',
@@ -41,5 +62,8 @@ ninja.data = [{
           description: "",
           section: "News",},{id: "news-we-are-happy-to-announce-the-program-and-that-maribel-acosta-tum-will-give-the-closing-keynote",
           title: 'We are happy to announce the program and that Maribel Acosta (TUM) will...',
+          description: "",
+          section: "News",},{id: "news-website-is-live",
+          title: 'Website is live.',
           description: "",
           section: "News",},];
